49a7abb3b40dc92a995d7b29a8bc5cce58ebc664
Tukenov <aslanban99@gmail.com>
2024-02-07 18:29:36 +0600

    La3

---
+++ 39.py
@@ -0,0 +1,26 @@
+#Function 
+#Ex 1
+def my_function():
+    print("Hello from a function")
+
+#Ex2
+def my_function():
+    print("Hello from a function")
+
+#Ex3
+    def my_function(fname, lname):
+        print(fname)
+
+#Ex4
+        def my_function(x):
+            return x+5
+        
+#Ex5
+        
+def my_function(*kids):
+  print("The youngest child is " + kids[2])
+  #Ex6
+  def my_function(*kids):
+     print("The youngest child is " + kids[2])
+
+
---
+++ 40.py
@@ -0,0 +1,54 @@
+#Lambda
+#Ex1
+x = lambda a:a
+
+#classes
+#Ex1
+class MyClass:
+    x=5
+#Ex2
+class MyClass:
+    x=5
+    p1 = MyClass()
+#Ex3
+class MyClass:
+    x=5
+    p1 = MyClass()
+    print(p1.x)
+#Ex4
+    class Person:
+        def __init__(self, name, age):
+            self.name = name
+            self.age = age
+
+#Inhe
+#Ex1
+class Student(Person):
+    #Ex2
+    class Person:
+        def __init__(self, fname):
+            self.firstname = fname
+            def printname(self):
+                print(self.firstname)
+                class Student(Person):pass
+                x = Student("Mike")
+                x.printname()
+
+#Modules
+#Ex1
+    import mymodule
+#Ex2
+    import mymodule as mx
+#Ex3
+    import mymodule
+    print(dir(mymodule))
+#Ex4
+from mymodule import person1    
+    
+    
+    
+
+
+    
+    
+    
